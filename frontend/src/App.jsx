c253c14ede82e3b09e20556db180c0d3773b0a01
abdulodhood003 <abdulodudood@gmail.com>
2025-09-24 19:03:03 +0530

    Fix Footer import casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 import Home from './pages/home'
 import Collection from './pages/collection'
