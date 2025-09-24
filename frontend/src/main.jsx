1e422af9d47b2f21b0a9c0592467114011566731
abdulodhood003 <abdulodudood@gmail.com>
2025-09-24 18:59:14 +0530

    Fix ShopContext import casing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import ShopContextProvider from './context/shopcontext'
+import ShopContextProvider from './context/ShopContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
